28c244941e2eb20f73a01d5ad0abd1dfd73fd491
YUUKIToriyama <github@toriyama.dev>
2022-08-31 15:29:15 +0900

    スレッドの中身も保存するようにする

--- src/Slarch.ts
+++ src/Slarch.ts
@@ -1,4 +1,5 @@
 import { ConversationsHistoryResponse, WebClient } from '@slack/web-api'
+import { Message as ResponseMessage } from "@slack/web-api/dist/response/ConversationsHistoryResponse"
 
 interface User {
 	userId: string
@@ -24,7 +25,7 @@ export class Slarch {
 		this.userList = []
 	}
 
-	resolveUserName = async (userId: string) => {
+	private resolveUserName = async (userId: string) => {
 		const user = await this.webClient.users.info({
 			user: userId
 		}).then(result => result.user)
@@ -62,20 +63,43 @@ export class Slarch {
 			nextCursor = response.response_metadata?.next_cursor
 			if (response.messages !== undefined) {
 				for (let message of response.messages) {
-					const dateTime = new Date(parseFloat(message.ts as string) * 1000)
-					let userName = this.getUserName(message.user as string)
-					if (userName === undefined) {
-						userName = await this.resolveUserName(message.user as string).catch(error => "hoge")
-					}
-					messages.push({
-						timestamp: dateTime.toLocaleString("ja-JP"),
-						userId: message.user || "",
-						userName: userName || "",
-						text: message.text || ""
+					// メッセージを保存:
+					await this.convertMessage(message).then(result => {
+						messages.push(result)
 					})
+					// スレッドになっている場合はそれも保存
+					if (message.thread_ts !== undefined) {
+						const threads = await this.webClient.conversations.replies({
+							ts: message.thread_ts,
+							channel: channelId
+						}).then(result => result.messages)
+						if (threads !== undefined) {
+							for (let thread of threads) {
+								await this.convertMessage(thread).then(result => {
+									messages.push(result)
+								})
+							}
+						}
+					}
 				}
 			}
 		}
 		return messages
+	}
+
+	private convertMessage = async (message: ResponseMessage) => {
+		// Unix Time -> 日本標準時
+		const dateTime = new Date(parseFloat(message.ts as string) * 1000)
+		// userId -> userName
+		let userName = this.getUserName(message.user as string)
+		if (userName === undefined) {
+			userName = await this.resolveUserName(message.user as string).catch(error => "hoge")
+		}
+		return {
+			timestamp: dateTime.toLocaleString("ja-JP"),
+			userId: message.user || "",
+			userName: userName || "",
+			text: message.text || ""
+		}
 	}
 }
